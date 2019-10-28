a51ca8b8bf18aa13741e8da76097eff8fd69c14a
Victor <vic8.taillieu@gmail.com>
2019-10-29 00:17:38 +0900

    Working basic GUI

--- ytdownload.py
+++ ytdownload.py
@@ -1,11 +1,31 @@
 import subprocess
-import os
+import tkinter as tk
 
-cmd = ["youtube-dl"]
-path = "C:/Users/vic8t/Desktop"
-audio_only = 1
-if audio_only:
-    cmd.append("-f m4a")
-cmd.append("https://www.youtube.com/watch?v=8BUig7mcFsw")
+def cmd(url, audioOnly):
+    cmd = ["youtube-dl"]
+    path = "C:/Users/vic8t/Desktop"
+    if audioOnly:
+        cmd.append("-f m4a")
+    cmd.append(url)
 
-subprocess.call(cmd, cwd=path)
+    subprocess.call(cmd, cwd=path)
+
+def set_url(url, inputUrl):
+    url.set(inputUrl.get())
+
+top = tk.Tk()
+top.title("YouTube downloader")
+
+url = tk.StringVar()
+audio = tk.IntVar()
+
+inputAudio = tk.Checkbutton(top, text = "Audio only", variable = audio, onvalue = 1, offvalue = 0)
+inputAudio.grid(row = 0, column = 1)
+
+inputUrl = tk.Entry(top, textvariable = url)
+inputUrl.grid(row = 1, column = 0)
+
+getUrl = tk.Button(top, text = "Download", command = lambda: cmd(inputUrl.get(), audio.get()))
+getUrl.grid(row = 1, column = 1)
+
+top.mainloop()
