2e0465d3ebb0854bfd44816835e0397923624bc3
Victor <vic8.taillieu@gmail.com>
2019-10-30 16:36:33 +0900

    Added subtitles option

--- ytdownload.py
+++ ytdownload.py
@@ -2,12 +2,14 @@ import subprocess
 import tkinter as tk
 from tkinter import filedialog
 
-def cmd(url, audioOnly, outputDir):
+def cmd():
     cmd = ["youtube-dl"]
-    path = outputDir
-    if audioOnly:
+    path = outputDir.get()
+    if audio.get():
         cmd.append("-f m4a")
-    cmd.append(url)
+    if sub.get():
+        cmd.append("--write-sub")
+    cmd.append(inputUrl.get())
 
     subprocess.call(cmd, cwd=path)
     # open confirmation window
@@ -47,8 +49,8 @@ frameVideo.place(relx=0.5, rely=0.4, relwidth=0.75, relheight=0.5, anchor='n')
 
 path = tk.StringVar()
 audio = tk.IntVar()
+sub = tk.IntVar()
 url = tk.StringVar()
-# status = tk.StringVar()
 
 getDir = tk.Button(frameDir, text="Choose directory", command=lambda: set_dir(path))
 getDir.place(relwidth=0.3, relheight=1)
@@ -62,8 +64,8 @@ scroll.config(command=outputDir.xview)
 inputAudio = tk.Checkbutton(frameVideo, text="Audio only", variable=audio, onvalue=1, offvalue=0)
 inputAudio.place(rely=0.1, relwidth=0.3, relheight=0.3)
 
-# showStatus = tk.Label(frameVideo, textvariable=status)
-# showStatus.place(relx=0.5, rely=0.1, relwidth=0.5, relheight=0.3)
+inputSub = tk.Checkbutton(frameVideo, text="Subtitles", variable=sub, onvalue=1, offvalue=0)
+inputSub.place(relx=0.4, rely=0.1, relwidth=0.3, relheight=0.3)
 
 inputUrl = tk.Entry(frameVideo, textvariable=url)
 inputUrl.insert(0, "Enter video URL...")
@@ -72,7 +74,7 @@ inputUrl.bind("<FocusOut>", on_focusout)
 inputUrl.config(fg="grey")
 inputUrl.place(rely=0.6, relwidth=0.7, relheight=0.3)
 
-getUrl = tk.Button(frameVideo, text="Download", activebackground="#ffa100", command=lambda: cmd(inputUrl.get(), audio.get(), outputDir.get()))
+getUrl = tk.Button(frameVideo, text="Download", activebackground="#ffa100", command=cmd)
 getUrl.place(relx=0.75, rely=0.6, relwidth=0.25, relheight=0.3)
 
 top.mainloop()
